77f07109f88d18ad2e42610fe810141bbf5f5376
jmGim <whdals5049@naver.com>
2021-12-08 19:59:45 +0900

    final_A

--- python/wav.py
+++ python/wav.py
@@ -125,6 +125,24 @@ def on_message(client, userdata, msg):
             stream.close()
 
             p.terminate()
+    elif int(msg.payload.decode('UTF-8')) == 7:
+        path = 'Kick1.wav'
+        with wave.open(path, 'rb') as f:
+            p = pyaudio.PyAudio()
+            stream = p.open(format=p.get_format_from_width(f.getsampwidth()),
+                            channels=f.getnchannels(),
+                            rate=f.getframerate(),
+                            output=True)
+
+            data = f.readframes(chunk)
+            while data:
+                stream.write(data)
+                data = f.readframes(chunk)
+
+            stream.stop_stream()
+            stream.close()
+
+            p.terminate()
 
 
 client = mqtt.Client()
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 
 #define speaker 14
 
+int loopcount = 0;
+
 int angle = 0; // mqtt에 전달하는 값
 
 const int MPU_ADDR = 0x68;                 // I2C통신을 위한 MPU6050의 주소
@@ -16,12 +18,7 @@ double angleFiX, angleFiY, angleFiZ;
 const double RADIAN_TO_DEGREE = 180 / 3.14159;
 const double DEG_PER_SEC = 32767 / 250; // 1초에 회전하는 각도
 const double ALPHA = 1 / (1 + 0.04);
-// GyX, GyY, GyZ 값의 범위 : -32768 ~ +32767 (16비트 정수범위)
 
-// int Track[10] = {"CloseHH.wav", "Crash.wav", "Crash1.wav", "Floor.wav",
-//                   "Kick1.wav", "Kick2.wav", "OpenHH.wav", "Ride.wav",
-//                    "Scare.wav", "Splash.wav", "Tom1.wav", "Tom2.wav", "Tom3.wav";
-//                    }
 unsigned long now = 0;  // 현재 시간 저장용 변수
 unsigned long past = 0; // 이전 시간 저장용 변수
 double dt = 0;          // 한 사이클 동안 걸린 시간 변수
@@ -33,9 +30,6 @@ const char *ssid = "Gogle";        // 희정 : KT_GiGA_2G_1F1E 연빈 : SK_WiFiG
 const char *password = "20010228"; // 희정 : dcgb2ed245  연빈: 2009024098
 const char *mqttServer = "3.84.34.84";
 const int mqttPort = 1883;
-
-unsigned long pubInterval = 1000;
-unsigned long lastPublished = -pubInterval;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -51,6 +45,7 @@ void initSensor()
 
 void getData()
 {
+    // delay(2000);
     Wire.beginTransmission(MPU_ADDR);
     Wire.write(0x3B); // AcX 레지스터 위치(주소)를 지칭합니다
     Wire.endTransmission(false);
@@ -62,6 +57,7 @@ void getData()
     GyX = Wire.read() << 8 | Wire.read();
     GyY = Wire.read() << 8 | Wire.read();
     GyZ = Wire.read() << 8 | Wire.read();
+    // delay(2000);
 }
 
 // loop 한 사이클동안 걸리는 시간을 알기위한 함수
@@ -139,6 +135,8 @@ void loop()
     getData();
     getDT();
 
+    int lux = analogRead(0);
+
     angleAcX = atan(AcY / sqrt(pow(AcX, 2) + pow(AcZ, 2)));
     angleAcX *= RADIAN_TO_DEGREE;
     angleAcY = atan(-AcX / sqrt(pow(AcY, 2) + pow(AcZ, 2)));
@@ -169,219 +167,78 @@ void loop()
     if (angleFiZ <= -90)
         angleFiZ = -90;
 
+    if (angleFiX >= 90)
+        angleFiX = 90;
+    if (angleFiX <= -90)
+        angleFiX = -90;
+
+    if (angleFiY >= 90)
+        angleFiY = 90;
+    if (angleFiY <= -90)
+        angleFiY = -90;
+
     Serial.print("\nAngleAcX:");
     Serial.print(angleAcX);
-    Serial.print("\t FilteredX:");
-    Serial.print(angleFiX);
+
     Serial.print("\n AngleAcY:");
     Serial.print(angleAcY);
-    Serial.print("\t FilteredY:");
-    Serial.print(angleFiY);
+
     Serial.print("\n AngleGyZ:");
     Serial.print(angleGyZ);
-    Serial.print("\t FilteredZ:");
-    Serial.println(angleFiZ);
 
-    // 파일 경로는 자신의 파일 경로로  변경
-    if (angleFiX <= 10)
+    if (angleGyZ >= 60 && angleGyZ < 90)
     {
-        if (angleGyZ >= -30 && angleGyZ < 0)
-        {
-            angle = 1;
-        }
-
-        else if (angleGyZ >= 0 && angleGyZ < 30)
-        {
-            angle = 2;
-        }
-
-        else if (angleGyZ >= -90 && angleGyZ < -60)
-        {
-            angle = 3;
-        }
-
-        else if (angleGyZ >= 60 && angleGyZ < 90)
-        {
-            angle = 4;
-        }
+        angle = 6;
+        // delay(50);
     }
 
-    else if (angleFiX <= 45)
+    else if (angleGyZ >= 30 && angleGyZ < 60)
     {
-        if (angleGyZ >= -60 && angleGyZ < -30)
-        {
-            angle = 5;
-        }
-
-        else if (angleGyZ >= 30 && angleGyZ < 60)
-        {
-            angle = 6;
-        }
+        angle = 5;
+        // delay(50);
     }
-    else
-        angle = 0;
+
+    else if (angleGyZ >= 0 && angleGyZ < 30)
+    {
+        angle = 4;
+        // delay(50);
+    }
+
+    else if (angleGyZ >= -30 && angleGyZ < 0)
+    {
+        angle = 3;
+        // delay(50);
+    }
+    // }
+    // else if (angleAcX < -20 && angleAcX >= -45)
+    // {
+    if (angleGyZ >= -60 && angleGyZ < -30)
+    {
+        angle = 2;
+        // delay(50);
+    }
+
+    else if (angleGyZ >= -90 && angleGyZ < -60)
+    {
+        angle = 1;
+        // delay(50);
+    }
+
+    else if (lux < 80)
+    {
+        angle = 7;
+        // Serial.printf("angle = %d\n", angle);
+    }
 
     Serial.print("현재 값: ");
     Serial.println(angle);
 
-    unsigned long currentMillis = millis();
-    if (currentMillis - lastPublished >= pubInterval)
+    loopcount++;
+    if (loopcount == 250)
     {
-        lastPublished = currentMillis;
         char buf[10];
         sprintf(buf, "%d", angle);
         client.publish("deviceid/team3/evt/angle", buf);
+        loopcount = 0;
     }
 }
-
-//////////////////////////////////////////////////////////////////////////////////////
-/* 유튜브 링크 코드 */
-// https://www.notion.so/IoT-1ce91dd2e1654c26aa90494557632e4b#9b810ee0da054a07bf10da85282f576b //
-
-// #include <Wire.h>
-// // #include "SD.h"
-// // #define SD_ChipSelectPin 4
-// // #include "TMRpcm.h"
-// #include "SPI.h"
-
-// // TMRpcm tmrpcm;
-
-// // int Track[10] = {"1.wav", "2.wav", "3.wav", "4.wav", "5.wav", "6.wav", "7.wav", "8.wav", "9.wav", "10.wav"};
-// int x = 5;
-
-// long gyroX, gyroY, gyroZ;
-// float rotX, rotY, rotZ, rotX1, rotY1, rotZ1;
-
-// void setupMPU()
-// {
-//   Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
-//   Wire.write(0x6B);                  //Accessing the register 6B - Power Management (Sec. 4.28)
-//   Wire.write(0b00000000);            //Setting SLEEP register to 0. (Required; see Note on p. 9)
-//   Wire.endTransmission();
-//   Wire.beginTransmission(0b1101000); //I2C address of the MPU
-//   Wire.write(0x1B);                  //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4)
-//   Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s
-//   Wire.write(0b00011000); //Setting the gyro to full scale +/- 2000deg./s
-//   Wire.endTransmission();
-//   Wire.beginTransmission(0b1101000); //I2C address of the MPU
-//   Wire.write(0x1C);                  //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5)
-//   //Wire.write(0b00000000); //Setting the accel to +/- 2g
-//   Wire.write(0b00011000); //Setting the accel to +/- 16g
-//   Wire.endTransmission();
-// }
-
-// void processGyroData()
-// {
-//   rotX = (gyroX / (131.0 * (2000 / 250))) - 0.019;
-//   rotY = (gyroY / (131.0 * (2000 / 250))) + 0.022;
-//   rotZ = (gyroZ / (131.0 * (2000 / 250))) - 0.015;
-
-//   if (rotZ <= 0.01 && rotZ >= -0.01)
-//   {
-//     rotZ = 0;
-//   }
-
-//   if (rotX >= 3 || rotX <= -3){
-//     rotZ=0;
-//   }
-
-//   rotX1 = rotX ;
-//   rotY1 = rotY ;
-//   rotZ1 = rotZ ;
-
-//   if (rotX1 >= 360) {
-//     rotX1 /= 360;
-//   }
-
-//   if (rotY1 >= 360) {
-//     rotY1 /= 360;
-//   }
-
-//   if (rotZ1 >= 360) {
-//     rotZ1 /= 360;
-//   }
-// }
-
-// void recordGyroRegisters()
-// {
-
-//   Wire.beginTransmission(0b1101000);
-//   Wire.write(0x43);
-//   Wire.endTransmission();
-//   Wire.requestFrom(0b1101000, 6);
-//   while (Wire.available() < 6)
-//     ;
-//   gyroX = Wire.read() << 8 | Wire.read();
-//   gyroY = Wire.read() << 8 | Wire.read();
-//   gyroZ = Wire.read() << 8 | Wire.read();
-//   processGyroData();
-// }
-
-// void printData()
-// {
-//   Serial.print("Gyro (deg)");
-//   Serial.print(" X=");
-//   Serial.print(rotX1);
-//   Serial.print(" Y=");
-//   Serial.print(rotY1);
-//   Serial.print(" Z=");
-//   Serial.println(rotZ1);
-//   Serial.printf("x: %d\n", x);
-// }
-// void setup()
-// {
-//   // tmrpcm.speakerPin = 9;
-//   Serial.begin(115200);
-//   Wire.begin();
-//   setupMPU();
-//   // if (!SD.begin(SD_ChipSelectPin)) {
-//   // Serial.println("SD fail");
-//   // return;
-//   // tmrpcm.setVolume(5);
-//   //tmrpcm.play("1.wav");
-//   // }
-// }
-
-// void loop()
-// {
-//   recordGyroRegisters();
-
-//   if (rotZ1 >= -15 && rotZ1 <= 15)
-//   {
-//     x = 5;
-//     delay(50);
-//   }
-
-//   if (rotZ1 >= -30 && rotZ1 <= -16)
-//   {
-//     x = 6;
-//     delay(50);
-//   }
-
-//   if (rotZ1 >= -45 && rotZ1 <= -31)
-//   {
-//     x = 7;
-//     delay(50);
-//   }
-
-//   if (rotZ1 >= 16 && rotZ1 <= 30)
-//   {
-//     x = 4;
-//     delay(50);
-//   }
-
-//   if (rotZ1 >= 31 && rotZ1 <= 45)
-//   {
-//     x = 3;
-//     delay(50);
-//   }
-//   if (rotX <= -5)
-//   {
-
-//     // tmrpcm.play(Track[x]);
-//     delay(50);
-//   }
-
-//   printData();
-//   delay(50);
-// }
